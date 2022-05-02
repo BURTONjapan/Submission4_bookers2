a9ef99e4053eeda38f57713a8a8ab9802f4819c0
BURTONjapan <kobayashi.hayato.infratop@gmail.com>
2022-05-02 11:54:41 +0000

    [finish]additional_task

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
+  get 'favorites/create'
+  get 'favorites/destroy'
   devise_for :users
   root to: "home#top"
-  resources :books, only: [:index, :show, :edit, :create, :destroy, :update]
+  resources :books, only: [:index, :show, :edit, :create, :destroy, :update] do
+    resource :favorites, only: [:create, :destroy]
+    resources :book_comments, only: [:create, :destroy]
+  end
+
   resources :users, only: [:show, :edit, :index, :update]
+
   get "home/about" => 'home#about', as: 'about'
   get "books/:id" => 'books#show', as: 'bookkwsk'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
